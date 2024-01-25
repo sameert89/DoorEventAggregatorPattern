62137af9fd6420cb26d3cfa8fd0141b37111f3ac
X6STRIVE <X6STRIVE@IN05N001NS>
2024-01-25 11:13:16 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorModelEventAggregatorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Buying Door
+            SmartDoor myDoorA = new SmartDoor();
+            // Subscribing Addons
+            var buzzerA = new Buzzer();
+            var pagerA = new PagerNotifier();
+            var autoCloseA = new AutoClose();
+            // Testing
+            myDoorA.Open();
+        }
+    }
+}
---
+++ SmartDoor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorModelEventAggregatorPattern
+{
+    public class SmartDoor : SimpleDoor
+    {
+        private Timer timerObj;
+        private Action<DoorStateChangeEvent> HandleDoorStateChangeEvent;
+        public SmartDoor(int t = 10)
+        {
+            timerObj = new Timer(t);
+            HandleDoorStateChangeEvent = (e) =>
+            {
+                Close();
+            };
+            DoorEventAggregator.Instance.Subscribe(HandleDoorStateChangeEvent);
+        }
+        public override bool Open()
+        {
+            if (base.Open())
+            {
+                DoorEventAggregator.Instance.Publish(new DoorStateChangeEvent(Constants.DoorStates.OPEN));
+                return true;
+            }
+            return false;
+        }
+        public override bool Close()
+        {
+            if (base.Close())
+            {
+                DoorEventAggregator.Instance.Publish(new DoorStateChangeEvent(Constants.DoorStates.CLOSED));
+                return true;
+            }
+            return false;
+        }
+    }
+}
